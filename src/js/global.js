acd1ca51437e906f2bd2febf191385dce46b8aa2
Don Townsend <don@teamairship.com>
2021-10-06 22:52:45 -0400

    generalize animation functions; add sideways text transforms

--- src/js/global.js
+++ src/js/global.js
@@ -15,74 +15,104 @@
     $input: null,
   };
 
-  window.onload = () => {
-    const elementsToTransform = document.querySelectorAll('[data-animate-mode="transform"]');
-    console.log(elementsToTransform);
-
-    const $p1 = document.getElementById("plx-text-1");
-    const $p2 = document.getElementById("plx-text-2");
-    const $p3 = document.getElementById("plx-text-3");
-    const $section = document.getElementById("title-section");
-    useAnimate($p1, $section, { x: 0, y: 0 }, { x: -1500, y: 0 });
-    useAnimate($p2, $section, { x: 0, y: 0 }, { x: 0, y: -400 });
-    useAnimate($p3, $section, { x: 0, y: 0 }, { x: 1500, y: 0 });
-    // setupParallaxSlider();
-    // setupParallaxText();
-    // setupAnimation();
-    // const [rect, reload] = useRect(document.getElementById("title-section"));
-    // state.min = rect.top;
-    // state.max =
+  window.onbeforeunload = () => {
+    window.scrollTo(0, 0);
   };
 
-  function transform($elem = null, x = 0, y = 0) {
-    if (!$elem) return;
-    $elem.style.transform = `translate(${x}px, ${y}px)`;
-  }
+  window.onload = () => {
+    setupAutoscroll();
+    setupTransformAnimations();
+  };
 
-  function onRangeChange(ev) {
-    if (!ev.target || !ev.target.value) return;
-    state.currentValue = Number(ev.target.value);
-  }
+  function setupTransformAnimations() {
+    const elementsToTransform = document.querySelectorAll('[data-animate-mode="transform"]');
+    if (!elementsToTransform || !elementsToTransform.length) return;
 
-  function setupParallaxSlider() {
-    const $input = (state.$input = document.getElementById("parallax-range-input"));
-    if (!$input) return;
+    elementsToTransform.forEach((element) => {
+      const {
+        animateMode,
+        animateScrollContainer,
+        animatePositionStart = "",
+        animatePositionEnd = "",
+        animateEasing,
+        debug,
+      } = element.dataset;
+      if (animateMode !== "transform") return;
+      if (!animateScrollContainer) return;
 
-    state.currentValue = Number($input.getAttribute("value") || 0) || 0;
-    state.min = Number($input.getAttribute("min") || 0) || 0;
-    state.max = Number($input.getAttribute("max") || 100) || 100;
-    $input.addEventListener("input", onRangeChange);
-  }
-
-  function setupParallaxText() {
-    state.$p1 = document.getElementById("plx-text-1");
-    state.$p2 = document.getElementById("plx-text-2");
-    state.$p3 = document.getElementById("plx-text-3");
-  }
-
-  function setupAnimation() {
-    let timestampPrevious = 0;
-    function loop(timestamp) {
-      if (timestamp === timestampPrevious) return;
-
-      if (state.currentValue !== state.previousValue) {
-        const { min, max, $p1, $p2, $p3 } = state;
-        const mul = (state.currentValue - min) / max;
-        const mulEased = easing.easeInQuart(mul, 0, 1, 1);
-        const mulInverse = min / max - mulEased + 1;
-
-        console.log(mulEased, mulInverse);
-
-        if ($p1) transform($p1, mulEased * constants.offsetP1.x, mulEased * constants.offsetP1.y);
-        if ($p2) transform($p2, mulEased * constants.offsetP2.x, mulEased * constants.offsetP2.y);
-        if ($p3) transform($p3, mulEased * constants.offsetP3.x, mulEased * constants.offsetP3.y);
-
-        state.previousValue = state.currentValue;
+      const containerNode = document.getElementById(String(animateScrollContainer).replace(/^\$/, ""));
+      if (!containerNode) {
+        console.warn(`container node not found for id: "$${animateScrollContainer}""`);
+        return;
       }
 
+      const [startX = 0, startY = 0] = parseCoordinates(animatePositionStart);
+      const [endX = 0, endY = 0] = parseCoordinates(animatePositionEnd);
+
+      if (debug && debug !== "false") {
+        console.log(`DEBUG nodeId=${element.id}`, { startX, startY, endX, endY });
+      }
+
+      if (!startX && !startY && !endX && !endY) return;
+
+      const easingFn = easing[animateEasing] || undefined;
+
+      useAnimateTransform(
+        element,
+        containerNode,
+        { x: startX, y: startY },
+        { x: endX, y: endY },
+        {
+          easingFn,
+          debug: Boolean(debug && debug !== "false"),
+        }
+      );
+    });
+  }
+
+  function setupAutoscroll() {
+    let timestampPrevious = 0;
+    let currentScrollY = 0;
+    const scrollSpeed = 7;
+    function autoScroll(timestamp = 0) {
+      if (timestamp === timestampPrevious) return;
+      currentScrollY += scrollSpeed;
+      window.scroll(0, currentScrollY);
       timestampPrevious = timestamp;
-      window.requestAnimationFrame(loop);
+      window.requestAnimationFrame(autoScroll);
     }
-    window.requestAnimationFrame(loop);
+
+    document.addEventListener("keyup", (e) => {
+      const key = parseKeyCode(e);
+      if (e.shiftKey && e.ctrlKey && (key === "Enter" || key === 13)) {
+        console.log("STARTING AUTO SCROLL");
+        window.requestAnimationFrame(autoScroll);
+      }
+    });
+  }
+
+  function parseKeyCode(event) {
+    if (event.key !== undefined) {
+      return event.key;
+    }
+    if (event.code !== undefined) {
+      return event.code;
+    }
+    if (event.keyIdentifier !== undefined) {
+      return event.keyIdentifier;
+    }
+    if (event.keyCode !== undefined) {
+      return event.keyCode;
+    }
+    return null;
+  }
+
+  function parseCoordinates(coordinates = "") {
+    const coordinateStripReg = /[{}\[\]\(\)]/g;
+    const [x = 0, y = 0] = String(coordinates)
+      .replace(coordinateStripReg, "")
+      .split(",")
+      .map((v) => Number(v));
+    return [x || 0, y || 0];
   }
 })();
